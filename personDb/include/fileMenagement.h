c5873d62188cbabaf435d5630d0253142b062cbd
Mkorewo <96120793+Mkorewo@users.noreply.github.com>
2024-01-12 17:36:52 +0100

    Update fileMenagement.h

--- personDb/include/fileMenagement.h
+++ personDb/include/fileMenagement.h
@@ -1,10 +1,12 @@
 #ifndef FILEMENAGEMENT_H
 #define FILEMENAGEMENT_H
+#include <stdbool.h>
 
 int len();
 char *readFile();
 void editFile(char **data,int ln);
 void delDataFile(char **data,int ln,int del);
-void saveFile(char name[50],char surname[50],char age[3]);//temp
+void saveFile(char name[50],char surname[50],char age[3]);
+void saveTax(char name[50],char surname[50],char tax[3],bool a);
 
 #endif // FILEMENAGEMENT_H
